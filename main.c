77038bbac987c2050e14e9c95378424329c0a245
Jamie Lentin <jm@lentin.co.uk>
2022-12-26 12:32:53 +0000

    main: Print sudoku using ESC/POS
    Print cells as double-height characters, and draw a grid using bitmaps
    / pipe characters.
    
    Before printing anything form bitmasks for whether right/bottom line of
    each cell should be bold. Use these when printing rows / horiz_line.
    
    In theory bold_right should only apply for upper half of horiz_line
    intersections. However for non-killer sudokus it makes no
    difference.
    
    The output isn't that well aligned, there's a sub-pixel difference
    between the position of the pipe and our intersection bitmap.

--- main.c
+++ main.c
@@ -2,7 +2,53 @@
 #include <hardware/structs/rosc.h>
 #include "hardware/adc.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "puzzles.h"
+
+/* Pull out internal game_state definition from solo.c */
+typedef unsigned char digit;
+struct block_structure {
+    int refcount;
+
+    /*
+     * For text formatting, we do need c and r here.
+     */
+    int c, r, area;
+
+    /*
+     * For any square index, whichblock[i] gives its block index.
+     *
+     * For 0 <= b,i < cr, blocks[b][i] gives the index of the ith
+     * square in block b.  nr_squares[b] gives the number of squares
+     * in block b (also the number of valid elements in blocks[b]).
+     *
+     * blocks_data holds the data pointed to by blocks.
+     *
+     * nr_squares may be NULL for block structures where all blocks are
+     * the same size.
+     */
+    int *whichblock, **blocks, *nr_squares, *blocks_data;
+    int nr_blocks, max_nr_squares;
+};
+struct game_state {
+    /*
+     * For historical reasons, I use `cr' to denote the overall
+     * width/height of the puzzle. It was a natural notation when
+     * all puzzles were divided into blocks in a grid, but doesn't
+     * really make much sense given jigsaw puzzles. However, the
+     * obvious `n' is heavily used in the solver to describe the
+     * index of a number being placed, so `cr' will have to stay.
+     */
+    int cr;
+    struct block_structure *blocks;
+    struct block_structure *kblocks;   /* Blocks for killer puzzles.  */
+    bool xtype, killer;
+    digit *grid, *kgrid;
+    bool *pencil;                      /* c*r*c*r elements */
+    bool *immutable;                   /* marks which digits are clues */
+    bool completed, cheated;
+};
 
 void fatal(const char *fmt, ...)
 {
@@ -38,6 +84,52 @@ uint32_t hwrand32()
     return random;
 }
 
+/* Print a horizontal line in ESC/POS speak: https://reference.epson-biz.com/modules/ref_escpos/index.php?content_id=88 */
+void horiz_line(unsigned char intersect_ch, uint8_t cells, uint16_t bold_right, uint16_t bold_bottom) {
+    uint8_t i, j, next_cnt = 29;
+    uint16_t len = (29 + 1) * cells + 2;
+
+    putchar(0x1b); putchar(0x2a);
+    putchar(0x00); /* 8 dot mode */
+    putchar(len & 255);  /* Length low bit */
+    putchar(len / 256);  /* Length high bit */
+
+    /* Initial bold line */
+    putchar(intersect_ch);
+    putchar(intersect_ch);
+    next_cnt = 28;
+
+    for (i = 0; i < cells; i++) {
+        for (j = 0; j < next_cnt; j++) {
+            putchar(bold_bottom & (1 << i) ? 0b00011000 : 0b00010000);
+        }
+
+        putchar(intersect_ch);
+        if (bold_right & (1 << i)) {
+            putchar(intersect_ch);
+            next_cnt = 28;
+        } else {
+            next_cnt = 29;
+        }
+    }
+}
+
+void vert_line(bool bold) {
+    /* Double-height - https://escpos.readthedocs.io/en/latest/font_cmds.html#select-character-size-1d-21-rel-phx */
+    putchar(0x1d); putchar(0x21); putchar(0x01);
+
+    putchar(' ');
+    if (bold) {
+        /* Double-strike mode - https://escpos.readthedocs.io/en/latest/font_cmds.html#select-double-strike-mode-1b-47-phx */
+        putchar(0x1b); putchar(0x47); putchar(0x01);
+    }
+    putchar('|');
+    if (bold) {
+        putchar(0x1b); putchar(0x47); putchar(0x00);
+    }
+    putchar(' ');
+}
+
 int main() {
     stdio_uart_init();
 
@@ -60,6 +152,12 @@ int main() {
         game_params *p;
         game_state *s;
         char *desc, *aux = NULL;
+
+        /* Init printer: https://escpos.readthedocs.io/en/latest/font_cmds.html#b40 */
+        printf("\x1b\x40");
+
+        /* Center-align: https://escpos.readthedocs.io/en/latest/layout.html#select-justification-1b-61-rel-phx */
+        printf("\x1b\x61\x01");
 
         /* Set difficulty according to knob position */
         uint8_t knob_pos = adc_read() / (4070 / 6); /* Range: 17..4068~4074 */
@@ -85,7 +183,54 @@ int main() {
         desc = thegame.new_desc(p, rs, &aux, false);
         s = thegame.new_game(NULL, p, desc);
 
-        printf("%s\r\n", thegame.text_format(s));
+        /* Minimal Line spacing - https://escpos.readthedocs.io/en/latest/layout.html#line-spacing-1b-33-rel */
+        printf("\x1b\x33"); putchar(0x00);
+
+        /* Output grid */
+        for (uint8_t y = 0; y < s->cr; y++) {
+            uint16_t bold_right = 0;
+            uint16_t bold_bottom = 0;
+
+            for (uint8_t x = 0; x < s->cr; x++) {
+                /* Should separator to right / bottom of this cell be bold? */
+                if (x == s->cr - 1 || s->blocks->whichblock[y*s->cr+x] != s->blocks->whichblock[y*s->cr+x+1]) {
+                    bold_right |= 1 << x;
+                }
+                if (y == s->cr - 1 || s->blocks->whichblock[y*s->cr+x] != s->blocks->whichblock[(y+1)*s->cr+x]) {
+                    bold_bottom |= 1 << x;
+                }
+            }
+
+            /* Initial horizontal / vertical lines */
+            if (y == 0) {
+                horiz_line(0b00011111, s->cr, bold_right, 0x1FF);
+            }
+
+            for (uint8_t x = 0; x < s->cr; x++) {
+                digit d = s->grid[y*s->cr+x];
+
+                if (x == 0) {
+                    vert_line(true);
+                }
+
+                /* Double-size characters - https://escpos.readthedocs.io/en/latest/font_cmds.html#select-character-size-1d-21-rel-phx */
+                putchar(0x1d); putchar(0x21); putchar(0x11);
+                if (d > 0) {
+                    printf("%d", d);
+                } else {
+                    putchar(' ');
+                }
+
+                vert_line(bold_right & (1 << x));
+            }
+            printf("\r\n");
+
+            horiz_line(y == (s->cr - 1) ? 0b11111000 : 0b11111111, s->cr, bold_right, bold_bottom);
+        }
+
+        /* Re-init to reset spacing, feed ready for tear */
+        printf("\x1b\x40\r\n\r\n");
+        stdio_flush();
 
         /* Wait 60s, print another one */
         sleep_ms(60000);
