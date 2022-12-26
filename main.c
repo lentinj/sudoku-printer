879605e6ae0db55021f0a7417358c4669da1356c
Jamie Lentin <jm@lentin.co.uk>
2022-12-26 12:30:23 +0000

    main: Set difficulty via. pot on ADC

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "pico/stdlib.h"
 #include <hardware/structs/rosc.h>
+#include "hardware/adc.h"
 #include <stdarg.h>
 #include "puzzles.h"
 
@@ -44,12 +45,38 @@ int main() {
     uint32_t seed = hwrand32();
     random_state *rs = random_new((void*)&seed, sizeof(seed));
 
+    /* Configure ADC to read knob */
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+    gpio_init(27);
+    gpio_set_dir(27, GPIO_OUT);
+    gpio_put(27, 1);
+
     while (true) {
         game_params *p;
         game_state *s;
         char *desc, *aux = NULL;
 
-        p = thegame.default_params();
+        /* Set difficulty according to knob position */
+        uint8_t knob_pos = adc_read() / (4070 / 6); /* Range: 17..4068~4074 */
+        char *name;
+        if (knob_pos == 0) {
+            thegame.fetch_preset(2, &name, &p);
+        } else if (knob_pos == 1) {
+            thegame.fetch_preset(3, &name, &p);
+        } else if (knob_pos == 2) {
+            thegame.fetch_preset(5, &name, &p);
+        } else if (knob_pos == 3) {
+            thegame.fetch_preset(6, &name, &p);
+        } else if (knob_pos == 4) {
+            thegame.fetch_preset(8, &name, &p);
+        } else if (knob_pos == 5) {
+            thegame.fetch_preset(9, &name, &p);
+        } else {
+            thegame.fetch_preset(2, &name, &p);
+        }
+        printf("%d: %s\r\n", knob_pos + 1, name);
 
         /* Generate a puzzle (this may take a while) */
         desc = thegame.new_desc(p, rs, &aux, false);
